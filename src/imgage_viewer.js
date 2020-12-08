ca055dddd71098042f7b5bdb0cc186235df4935d
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-12-08 19:25:10 +0000

    image loaders

--- src/imgage_viewer.js
+++ src/imgage_viewer.js
@@ -1,5 +1,14 @@
-import '../directory/image_viewer.css'
+import big from '../assets/big.jpg';
+import small from '../assets/small.jpg';
+
+import '../directory/image_viewer.css';
 
 const image = document.createElement('img');
-image.src = 'http://lorempixel.com/400/400'
+image.src = 'http://lorempixel.com/400/400';
 document.body.appendChild(image);
+
+
+const bigImage = document.createElement('img');
+bigImage.src = small;
+document.body.appendChild(bigImage);
+
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: 'build/'
   },
   module: {
     rules: [
@@ -18,12 +19,20 @@ const config = {
           loader: 'css-loader'
         }),
         test: /\.css$/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          'image-webpack-loader'
+        ]
       }
     ]
   },
-  plugins: [
-    new ExtractTextPlugin('style.css')
-  ]
+  plugins: [new ExtractTextPlugin('style.css')]
 };
 
 module.exports = config;
