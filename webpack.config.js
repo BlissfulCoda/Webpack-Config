3ff4a7697727df75a32fb6bace4078f417341f0b
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-12-08 06:17:39 +0000

    downloaded babel-core, babel-loader, babel-preset-env and set module rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
-const path = require('path')
+const path = require('path');
 
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
-    }
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+      rule: [
+          {
+              use: 'babel-loader'
+          }
+      ]
+  }
 };
 
 module.exports = config;
